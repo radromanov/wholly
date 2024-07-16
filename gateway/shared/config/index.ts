11750766668bb03d0515909fc52477e4b87760b3
Radostin Romanov <rad.romanov@gmail.com>
2024-07-16 09:35:38 +0300

    refactor: initial clean up

--- gateway/shared/config/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./auth.service.config";
-export * from "./gateway.service.config";
--- gateway/shared/utils/zod.ts
+++
@@ -1,7 +0,0 @@
-export const required = (name: string = "String") => {
-  return { required_error: `${name} is required` };
-};
-export const minimum = (name: string, min = 1) =>
-  `${name} must be at least ${min} character(s)`;
-export const maximum = (name: string, max: number) =>
-  `${name} cannot exceed ${max} character(s)`;
