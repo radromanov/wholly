5650d7e2707b84dfd50519b2de56b752c5761575
Radostin Romanov <rad.romanov@gmail.com>
2024-07-16 10:47:09 +0300

    fix: husky config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+console.log("Testing if husky config works");
+
 app.listen(3001, () =>
   console.log(`Auth Microservice running on port ${3001}`),
 );
