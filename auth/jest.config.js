26c5be32cebf046bd26c660cbbb69b249ceeaa91
Radostin Romanov <rad.romanov@gmail.com>
2024-07-17 09:47:14 +0300

    refactor: shared/ entry directory; feat: add shared/tests/ and jest

--- auth/jest.config.js
+++ auth/jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
     "^@core(.*)$": "<rootDir>/src/core$1",
     "^@api(.*)$": "<rootDir>/src/api$1",
     "^@lib(.*)$": "<rootDir>/src/lib$1",
-    "^@shared(.*)$": "<rootDir>/../shared$1",
+    "^@shared(.*)$": "<rootDir>/../shared/src$1",
   },
 };
