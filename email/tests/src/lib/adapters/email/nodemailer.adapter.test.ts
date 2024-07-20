c258c6c1955dac21ee8dff220b317f02c23de36e
Radostin Romanov <rad.romanov@gmail.com>
2024-07-20 12:33:12 +0300

    refactor: email/lib/adapters/nodemailer.adapter test suite

--- email/tests/src/lib/adapters/email/nodemailer.adapter.test.ts
+++ email/tests/src/lib/adapters/email/nodemailer.adapter.test.ts
@@ -16,4 +16,26 @@ describe("Nodemailer Adapter", () => {
       });
     });
   });
+
+  describe("Environment", () => {
+    it("should correctly initialize in production environment", () => {
+      process.env.NODE_ENV = "production";
+      const nodemailerAdapter = new NodemailerAdapter();
+
+      expect(nodemailerAdapter["isSandbox"]).toBe(false);
+    });
+
+    it("should correctly initialize in development environment", () => {
+      process.env.NODE_ENV = "development";
+      const nodemailerAdapter = new NodemailerAdapter();
+
+      expect(nodemailerAdapter["isSandbox"]).toBe(true);
+    });
+
+    it("should correctly initialize in test environment", () => {
+      const nodemailerAdapter = new NodemailerAdapter();
+
+      expect(nodemailerAdapter["isSandbox"]).toBe(true);
+    });
+  });
 });
